d4cc7be00567381b3cf0bd99199205aaef79448e
Rinto-Torishima <paru.22.pen@icloud.com>
2023-03-02 16:20:15 +0900

    変更

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -7,15 +7,27 @@
                 {{ $error }}
             @endforeach
         @endif
-        <div class="form">
-            <form action="{{ route('topics.store') }}" method="POST">
-                @csrf
-                <input type="hidden" name="user_id" value={{ $idd }}>
-                <input id="ftop" class="form-control" type="text" name="name" value="{{ old('name') }}"
-                    placeholder="名前" required>
-                <textarea id="ftop" class="form-control" name="content" rows="3" placeholder="コメント" required>{{ old('content') }}</textarea>
-                <input class="btn btn-outline-primary" type="submit" value="送信">
-            </form>
-        </div>
+        @if (!Auth::check())
+            <div class="card">
+                <div style="text-align: center" class="card-body bg-light">
+                    ログインするとトピックを投稿することができます </div>
+            </div>
+        @elseif(Auth::user() && !Auth::user()->hasVerifiedEmail())
+            <div class="card">
+                <div style="text-align: center" class="card-body bg-light">
+                    メール認証を完了するとトピックを投稿することができます </div>
+            </div>
+        @else
+            <div class="form">
+                <form action="{{ route('topics.store') }}" method="POST">
+                    @csrf
+                    <input type="hidden" name="user_id" value={{ $idd }}>
+                    <input id="ftop" class="form-control" type="text" name="name" value="{{ old('name') }}"
+                        placeholder="名前" required>
+                    <textarea id="ftop" class="form-control" name="content" rows="3" placeholder="コメント" required>{{ old('content') }}</textarea>
+                    <input class="btn btn-outline-primary" type="submit" value="送信">
+                </form>
+            </div>
+        @endif
     </div>
 @endsection
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -64,23 +64,35 @@
 
 
         {{-- 送信 --}}
-        <div class="form">
-            <h2>コメント送信</h2>
-            <form action="{{ route('cmt.store') }}" method="POST">
-                @csrf
-                @if (count($errors))
-                    @foreach ($errors->all() as $error)
-                        {{ $error }}
-                    @endforeach
-                @endif
-                <input type="hidden" name="user_id" value={{ $idd }}>
-                <input type="hidden" name="topic_id" value={{ $topic->id }}>
-                <input id="ftop" class="form-control" value="{{ old('comment_name') }}" type="text"
-                    name="comment_name" placeholder="投稿者" required>
-                <textarea id="ftop" rows="3" class="form-control" name="comment_message" placeholder="投稿内容" required>{{ old('comment_message') }}</textarea>
-                <input class="btn btn-outline-primary" type="submit" value="返信">
-            </form>
-        </div>
+        @if (!Auth::check())
+            <div class="card">
+                <div style="text-align: center" class="card-body bg-light">
+                    ログインするとコメントを送信することができます
+                </div>
+            </div>
+        @elseif(Auth::user() && !Auth::user()->hasVerifiedEmail())
+            <div style="text-align: center" class="card-body bg-light">
+                メール認証を完了するとコメントを送信することができます
+            </div>
+        @else
+            <div class="form">
+                <h2>コメント送信</h2>
+                <form action="{{ route('cmt.store') }}" method="POST">
+                    @csrf
+                    @if (count($errors))
+                        @foreach ($errors->all() as $error)
+                            {{ $error }}
+                        @endforeach
+                    @endif
+                    <input type="hidden" name="user_id" value={{ $idd }}>
+                    <input type="hidden" name="topic_id" value={{ $topic->id }}>
+                    <input id="ftop" class="form-control" value="{{ old('comment_name') }}" type="text"
+                        name="comment_name" placeholder="投稿者" required>
+                    <textarea id="ftop" rows="3" class="form-control" name="comment_message" placeholder="投稿内容" required>{{ old('comment_message') }}</textarea>
+                    <input class="btn btn-outline-primary" type="submit" value="返信">
+                </form>
+            </div>
+        @endif
     </div>
 
 @endsection
