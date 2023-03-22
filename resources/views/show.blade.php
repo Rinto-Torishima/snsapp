da737878cb698b85aad8282e30af8395acea6212
Rinto-Torishima <paru.22.pen@icloud.com>
2023-03-22 20:35:38 +0900

    変更

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -83,7 +83,9 @@
                     @csrf
                     @if (count($errors))
                         @foreach ($errors->all() as $error)
-                            {{ $error }}
+                            <div class="alert alert-danger" role="alert">
+                                {{ $error }}
+                            </div>
                         @endforeach
                     @endif
                     <input type="hidden" name="user_id" value={{ $idd }}>
