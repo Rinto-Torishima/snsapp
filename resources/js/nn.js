4a46eaeb164a3320f9a0743627a931c5f5ce5f02
rinto torishima <paru.22.pen@icloud.com>
2024-08-10 18:54:31 +0900

    Update: ajax通信に失敗した際のエラー処理を追加

--- resources/js/nn.js
+++ resources/js/nn.js
@@ -2,29 +2,30 @@
 $(function () {
     let likeCommentId;
     $(document).on("click", ".like-toggle", function () {
-        //onはイベントハンドラー
-        let $this = $(this); //this=イベントの発火した要素＝iタグを代入
-        likeCommentId = $this.data("comment-id"); //iタグに仕込んだdata-review-idの値を取得
+        let $this = $(this);
+        likeCommentId = $this.data("comment-id");
         $.ajax({
             headers: {
-                //HTTPヘッダ情報をヘッダ名と値のマップで記述
                 "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr("content"),
-            }, //↑name属性がcsrf-tokenのmetaタグのcontent属性の値を取得
-            url: "/like", //通信先アドレスで、このURLをあとでルートで設定します
+            }, 
+            url: "/like",
             method: "POST",
             data: {
-                //サーバーに送信するデータ
-                comment_id: likeCommentId, //いいねされた投稿のidを送る
+                //いいねされた投稿のidを送る
+                comment_id: likeCommentId, 
             },
         })
-            //通信成功した時の処理
             .done(function (data) {
-                $this.toggleClass("liked"); //likedクラスのON/OFF切り替え。
+                $this.toggleClass("liked");
+                // いいねの数をリアルタイムで更新
                 $this.next(".like-counter").html(data.comment_likes_count);
             })
-            //通信失敗した時の処理
-            .fail(function () {
-                console.log("fail");
+            .fail(function (jqXHR, textStatus, errorThrown) {
+                alert("いいねの処理に失敗しました。もう一度お試しください。");
+                console.log("ajax通信に失敗しました");
+                console.log("jqXHR          : " + jqXHR.status);
+                console.log("textStatus     : " + textStatus);
+                console.log("errorThrown    : " + errorThrown.message);
             });
     });
 });
